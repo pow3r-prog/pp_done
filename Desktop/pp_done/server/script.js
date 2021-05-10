851cf501e72be0615fd4e217cdd33e565483e741
P0w3rr <vova.andreythuk40@gmail.com>
2021-05-10 23:31:06 +0300

    server commit

---
+++ Desktop/pp_done/server/script.js
@@ -0,0 +1,9 @@
+let block = document.querySelector('.block');
+let text = document.getElementById('text');
+
+
+function buttonBlock() {
+    block.style.cssText = 'background-color: blue; color: white;';
+    text.innerHTML="Сервер зроблейний на Python";
+}
+
